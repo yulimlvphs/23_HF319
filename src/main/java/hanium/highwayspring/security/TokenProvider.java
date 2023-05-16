f6058a15e81af5427f33e0aaf1d948f3f08e0e9a
yulim <yulimlvphs@gmail.com>
2023-05-16 10:18:02 +0900

    JwtAuthenticationFilter, TokenProvider, UserDTO 3개 파일 수정

--- src/main/java/hanium/highwayspring/security/TokenProvider.java
+++ src/main/java/hanium/highwayspring/security/TokenProvider.java
@@ -4,6 +4,7 @@ import hanium.highwayspring.user.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -39,8 +40,15 @@ public class TokenProvider {
         // JWT Token 생성
         return Jwts.builder()
                 // header에 들어갈 내용 및 서명을 하기 위한 SECRET_KEY
-                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)//기존에 .signWith에서 오류나길래 이걸로 바꿨어
                 // payload에 들어갈 내용
+                .claim("user_no", userEntity.getId())
+                .claim("user_id", userEntity.getUid())
+                .claim("user_name", userEntity.getName())
+                .claim("user_email", userEntity.getEmail())
+                .claim("user_gender", userEntity.getGender())
+                .claim("user_age", userEntity.getAge())
+                .claim("user_role", userEntity.getRole())
                 .setSubject(userEntity.getUid()) // sub
                 .setIssuer("demo app")            // iss
                 .setIssuedAt(new Date())        // iat
@@ -59,5 +67,16 @@ public class TokenProvider {
                 .getBody();
 
         return claims.getSubject(); // subject 즉 사용자 아이디를 리턴한다.
+    }
+
+    /**
+     * 토큰의 Claim 디코딩
+     */
+    public Claims getAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
--- src/main/java/hanium/highwayspring/user/UserDTO.java
+++ src/main/java/hanium/highwayspring/user/UserDTO.java
@@ -4,14 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class UserDTO {
+public class UserDTO implements UserDetails {
     private Long userNo;
     private String userId;
     private String token;
@@ -20,6 +23,9 @@ public class UserDTO {
     private String userGender;
     private Long userAge;
     private Long userRole;
+    private boolean locked;	//계정 잠김 여부
+    private Collection<GrantedAuthority> authorities; //권한 목록
+
 
     public UserDTO(final User entity) {
         this.userNo = entity.getId();
@@ -42,4 +48,40 @@ public class UserDTO {
                 .userRole(entity.get().getRole())
                 .build();
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {//계정의 권한 목록을 리턴
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() { //계정의 비밀번호를 리턴
+        return null;
+    }
+
+    @Override
+    public String getUsername() { //계정의 고유한 값을 리턴 (ex. DB의 PK값)
+        return userId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { //계정의 만료 여부 리턴
+        return true; // 만료 안됨
+    }
+
+    @Override
+    public boolean isAccountNonLocked() { //계정의 잠김 여부 리턴
+        return true; //잠기지 않음
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() { //비밀번호 만료 여부 리턴
+        return true; //만료 안됨
+    }
+
+    @Override
+    public boolean isEnabled() { //계정의 활성화 여부 리턴
+        return true; // 활성화
+    }
 }
+
