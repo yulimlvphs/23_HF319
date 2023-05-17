2e41a300ae4702689ad61a1fe324d6c6df79fd0b
ljs001022 <ljs48425707@gmail.com>
2023-05-17 16:52:04 +0900

    edit : token : test

--- src/main/java/hanium/highwayspring/user/User.java
+++ src/main/java/hanium/highwayspring/user/User.java
@@ -16,7 +16,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-//    private String token;
     private String uid;
     private String pwd;
     private String name;
