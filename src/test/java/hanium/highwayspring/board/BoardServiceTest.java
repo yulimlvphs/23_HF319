ec7c96e9d0964487223060d3168d1a8b784a387a
ljs001022 <ljs48425707@gmail.com>
2023-06-12 16:30:23 +0900

    Create BoardServiceTest.java

---
+++ src/test/java/hanium/highwayspring/board/BoardServiceTest.java
@@ -0,0 +1,63 @@
+package hanium.highwayspring.board;
+
+import hanium.highwayspring.comment.CommentRequestDto;
+import hanium.highwayspring.comment.CommentService;
+import hanium.highwayspring.school.School;
+import hanium.highwayspring.school.SchoolService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
+//@Transactional
+class BoardCommentServiceTest {
+    @Autowired
+    BoardService boardService;
+    @Autowired
+    CommentService commentService;
+    @Autowired
+    SchoolService schoolService;
+
+    @Test
+    void 글작성() {
+        School school = schoolService.findBySchoolId(1L)
+                .orElseThrow(() -> new IllegalArgumentException("학교가 존재하지 않습니다."));
+        String uid = "admin";
+        String title = "제목";
+        String content = "내용";
+        Long category = 1L;
+        Board board = Board.builder()
+                .userId(uid)
+                .school(school)
+                .title(title)
+                .content(content)
+                .category(category)
+                .build();
+        boardService.create(board);
+    }
+
+    @Test
+    void 댓글작성(){
+        String uid = "admin";
+        String content = "댓글";
+        List<Board> boards = boardService.retrieve(uid);
+        System.out.println(boards.get(0).getId());
+        CommentRequestDto dto = CommentRequestDto.builder()
+                .userId(uid)
+                .boardId(boards.get(0).getId())
+                .content(content)
+                .build();
+        commentService.createComment(dto);
+    }
+
+    @Test
+    void 글삭제() {
+        String uid = "admin";
+        /*List<Board> boards = boardService.retrieve(uid);
+        boardService.delete(boards.get(0));*/
+        boardService.delete(boardService.findById(38L));
+    }
+}
