a96fee581e2a51d92931c3b0918223e1aef54fe0
yulim <yulimlvphs@gmail.com>
2023-06-22 12:53:07 +0900

    review - role 기능 추가

--- src/main/java/hanium/highwayspring/review/Review.java
+++ src/main/java/hanium/highwayspring/review/Review.java
@@ -24,13 +24,14 @@ public class Review{
     @JoinColumn(name = "userid")
     private User userId;//리뷰 작성자의 인덱스 값
     private String userName; //리뷰 작성자의 name
+    private Long role; //0이면 학생 or 1이면 교사
     private String tags; //리뷰 작성한 사람의 학과
     private String content; //리뷰 내용
     private Integer trafficRate; //별점 리뷰
     private Integer facilityRate;//별점 리뷰
-    private Integer cafeteriaRate;//별점 리뷰
+    private Integer cafeteriaRate;//급식 별점 리뷰
     private Integer educationRate;//별점 리뷰
-    private Integer employmentRate;//별점 리뷰
+    private Integer employmentRate;//교사 별점 리뷰
     @ColumnDefault("false")
     private boolean isDeleted;//논리적 삭제 여부. ture -> 논리적 삭제
     @ManyToOne
