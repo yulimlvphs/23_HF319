a694ee5799ce2830a6d70f312ef6600577c1a943
ljs001022 <ljs48425707@gmail.com>
2023-07-10 12:25:54 +0900

    Update ReviewController.java

--- src/main/java/hanium/highwayspring/review/ReviewController.java
+++ src/main/java/hanium/highwayspring/review/ReviewController.java
@@ -37,8 +37,8 @@ public class ReviewController {
     }
 
     //해당 학교애 대한 전체 리뷰 보여주기
-    @GetMapping("/list")
-    public ResponseDTO<?> getReviewsBySchoolId(@RequestParam(name = "schoolId") Long schoolId) {
+    @GetMapping("/list/{id}")
+    public ResponseDTO<?> getReviewsBySchoolId(@PathVariable(name = "id") Long schoolId) {
         School school = schoolService.findBySchoolId(schoolId)
                 .orElseThrow(() -> new IllegalArgumentException("학교가 존재하지 않습니다."));
         return reviewService.findAll(school);
