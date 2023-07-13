5ebd855aa1b1025476224bb1008b8e9a3f6c93d9
yulim <yulimlvphs@gmail.com>
2023-07-13 14:42:02 +0900

    주석추가

--- src/main/java/hanium/highwayspring/user/UserRepository.java
+++ src/main/java/hanium/highwayspring/user/UserRepository.java
@@ -6,5 +6,4 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByUid(String user_id);
-    long countBySchoolId(int schoolId);
 }
