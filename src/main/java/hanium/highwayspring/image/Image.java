0f7ffee1809b31424dfce01a62d41b8c9622825c
yulim <yulimlvphs@gmail.com>
2023-07-26 22:59:03 +0900

    - S3 버킷에 이미지 업로드 코드(극극초안)

---
+++ src/main/java/hanium/highwayspring/image/Image.java
@@ -0,0 +1,27 @@
+package hanium.highwayspring.image;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity(name = "image_TB")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;  // 일기 pk
+
+    @Column
+    private String imageUrl;
+
+    public Image(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
