f65a7ebe7e24ddec0140091e79f569301413b445
yulim <yulimlvphs@gmail.com>
2023-08-30 22:09:32 +0900

    git 코드 꼬임 수정 + aws 오류 수정

--- src/main/java/hanium/highwayspring/school/heart/repository/SchoolHeartRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package hanium.highwayspring.school.heart.repository;
-
-import hanium.highwayspring.config.res.ResponseDTO;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface SchoolHeartRepositoryCustom {
-    public ResponseDTO<?> findAll(Long userId);
-}
